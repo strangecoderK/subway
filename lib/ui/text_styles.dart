df5b1d021217864ded98fdcee18dc0660991aa7c
2021itb <syw7777@naver.com>
2024-08-16 19:41:21 +0900

    feat: create text field component

---
+++ lib/ui/text_styles.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+abstract class TextStyles {
+  static const TextStyle titleTextBold =TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 50,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle headerTextBold =TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle largeTextBold =  TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle mediumTextBold = TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle normalTextBold = TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle smallTextBold =  TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle smallerTextBold = TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 11,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle tittleTextRegular = TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 50,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const  TextStyle headerTextRegular =TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 30,
+    fontWeight: FontWeight.normal,
+  );
+
+  static  const TextStyle largeTextRegular = TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle mediumTextRegular =TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 18,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle normalTextRegular =  TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle smallTextRegular =  TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle smallerTextRegular =  TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 11,
+    fontWeight: FontWeight.normal,
+  );
+}
